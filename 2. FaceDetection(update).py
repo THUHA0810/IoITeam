2c7ab2b080f4f5406bd7e936f9459dea2dd87346
THUHA0810 <94898194+THUHA0810@users.noreply.github.com>
2022-01-14 09:58:31 +0700

    Update 2. FaceDetection(update).py

--- 2. FaceDetection(update).py
+++ 2. FaceDetection(update).py
@@ -1,4 +1,4 @@
-import cv2, os, sqlite3
+import cv2, os, sqlite3, pyautogui
 import numpy as np
 from PIL import Image
 
@@ -20,30 +20,28 @@ def getProfile(id):
     conn.close()
     return profile
 
-cap = cv2.VideoCapture(0)
 fontface = cv2.FONT_HERSHEY_SIMPLEX
 
 while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    screenshot = pyautogui.screenshot()
+    screenshot = np.array(screenshot)
+
+    gray = cv2.cvtColor(screenshot, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray)
 
     for(x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.rectangle(screenshot, (x, y), (x + w, y + h), (0, 255, 0), 2)
         id,conf = recognizer.predict(gray[y:y+h,x:x+w])
 
         if conf < 40:
             profile = getProfile(id)
 
             if profile != None:
-                cv2.putText(frame, "" + str(profile[1]), (x + 10, y + h + 30), fontface, 1, (0, 255, 0), 2)
-        
+                cv2.putText(screenshot, "" + str(profile[1]), (x + 10, y + h + 30), fontface, 1, (0, 255, 0), 2)
+                print(str(profile[0]) + "-" + str(profile[1]))
         else:
-            cv2.putText(frame, "Unknow", (x + 10, y + h + 30), fontface, 1, (0, 0, 255), 2)
+            cv2.putText(screenshot, "Unknow", (x + 10, y + h + 30), fontface, 1, (0, 0, 255), 2)
 
-    cv2.imshow('Face detection', frame)
+    cv2.imshow('Face detection', screenshot)
     if cv2.waitKey(1) == ord('q'):
         break
-
-cap.release()
-cv2.destroyAllWindows()
